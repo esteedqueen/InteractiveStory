37b2a08d37e68b2ffc16a6f0c16ea37cf3ca5cdb
Esther Olatunde <olatundeesther@gmail.com>
2015-09-12 21:59:59 +0100

    Huge initial commit - add page, story and choice data models

---
+++ app/src/main/java/com/amber/esther/interactivestory/ui/StoryActivity.java
@@ -0,0 +1,32 @@
+package com.amber.esther.interactivestory.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amber.esther.interactivestory.R;
+
+public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra(getString(R.string.key_name));
+
+        if (name == null) {
+            name = "Friend";
+        }
+
+        Log.d(TAG, name);
+    }
+
+
+
+}
